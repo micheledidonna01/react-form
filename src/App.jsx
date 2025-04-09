a7160f69fb3a96341ce8b873dcb963871abb2690
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-09 20:38:19 +0200

    feat: bonus 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,79 @@ import articles from './data/articles'
 
 function App() {
   const [nameArticle, setNameArticle] = useState(articles);
-  const [newArticle, setNewArticle] = useState('');
-
+  let [newArticle, setNewArticle] = useState('');
   
-  const addArticle = e => {
+
+
+  const addArticle = (e) => {
     e.preventDefault();
-    setNameArticle([...nameArticle, newArticle]);
-    alert('Form inviato!')
-    console.log(nameArticle);
+    return newArticle === "" ? alert('Inserisci il nome di un articolo!!') : setNameArticle([...nameArticle, newArticle]);
+    // alert('Form inviato!')
+    // console.log(nameArticle);
+    // setNameArticle([...nameArticle, newArticle]);
+    // alert('Form inviato!')
+    // console.log(nameArticle);
+
   }
 
   const removeArticle = index => {
+    console.log(index);
     const searchArticleToEliminate = nameArticle.filter((article, i) => {
+
       return i !== index;
     });
+    console.log(searchArticleToEliminate);
     setNameArticle(searchArticleToEliminate);
+  }
+
+  const modifyArticleFn = (index) => {
+    console.log(index);
+    console.log(nameArticle);
+    console.log(newArticle);
+    if(newArticle === ""){
+      return alert('Inserisci un testo per la modifica');
+    }    
+    const searchArticleToModify = nameArticle.map((article, i) => {
+      return index === i ? article = newArticle : article = `${article}`;
+    });
+
+    console.log(searchArticleToModify);  
+    return setNameArticle(searchArticleToModify);
+  
+    // setNewArticle(searchArticleToModify);
   }
 
 
   return (
     <>
-    <div className='container my-5'>
-      <ul className="list-group">
-        {nameArticle.map((article, index) => 
-        // <ListArticle key={index} title={nameArticle[index]} removeArticle = {removeArticle} setNameArticle = {setNameArticle}/>
-          <li key={index} className='list-group-item d-flex justify-content-between'>
-            <p>{nameArticle[index]}</p> 
-            <div className="buttons">
-                <button className="btn btn-outline-success">Modifica</button>  
-                <button className="btn btn-outline-danger" onClick={() => removeArticle(index)}>x</button>
-            </div>
-         </li>
-        )}
-      </ul>
-    </div>
+      <div className='container my-5'>
+        <ul className="list-group">
+          {nameArticle.map((article, index) =>
+            // <ListArticle key={index} title={nameArticle[index]} removeArticle = {removeArticle} setNameArticle = {setNameArticle}/>
+            <li key={index} className='list-group-item d-flex justify-content-between gap-2'>
+              <p>{nameArticle[index]}</p>
+              <div className="buttons d-flex justify-content-between gap-2">
+                <button className="btn btn-outline-success col-6" onClick={() => modifyArticleFn(index)} > M </button>
 
-    <form className='container d-flex row mx-auto gap-2' onSubmit={addArticle}>
-      <input type='text' 
-        placeholder='nome articolo'
-        className='form-control'
-        value={newArticle}
-        onChange={e => {
-          setNewArticle(e.target.value)
-        }}
-      />
-      <button className='btn btn-outline-primary'>Inserisci articolo</button>
-    </form>
+                <button className="btn btn-outline-danger" onClick={() => removeArticle(index)}>x</button>
+              </div>
+            </li>
+          )}
+        </ul>
+
+      </div>
+
+      <form className='container d-flex row mx-auto gap-2' onSubmit={addArticle}>
+        <input type='text'
+          placeholder='nome articolo'
+          className='form-control'
+          value={newArticle}
+          onChange={e => {
+            setNewArticle(e.target.value)
+          }}
+        />
+        <button className='btn btn-outline-primary'>Inserisci articolo</button>
+      </form>
     </>
   )
 }
