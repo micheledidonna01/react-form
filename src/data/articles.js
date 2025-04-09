c8642c76acb7582e0416272ba7b1adf3e4fff9b5
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-09 13:58:45 +0200

    feat: Milestone 1

---
+++ src/data/articles.js
@@ -0,0 +1,25 @@
+let articles = [
+    {
+        Id: 1,
+        title: "Articolo 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi labore aliquid itaque? Harum officia temporibus sunt nemo tempora aliquam magnam!"
+    },
+    {
+        Id: 2,
+        title: "Articolo 2",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi labore aliquid itaque? Harum officia temporibus sunt nemo tempora aliquam magnam!"
+    },
+    {
+        Id: 3,
+        title: "Articolo 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi labore aliquid itaque? Harum officia temporibus sunt nemo tempora aliquam magnam!"
+    },
+    {
+        Id: 4,
+        title: "Articolo 4",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi labore aliquid itaque? Harum officia temporibus sunt nemo tempora aliquam magnam!"
+    }
+];
+
+
+export default articles
